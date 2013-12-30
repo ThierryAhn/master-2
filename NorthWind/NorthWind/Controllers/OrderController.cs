518aae7c86d38743529b1dc4cc1cd7c8436cef27
folabi41 <folabi41@gmail.com>
2013-12-30 17:27:54 +0000

    Import initial

---
+++ NorthWind/NorthWind/Controllers/OrderController.cs
@@ -0,0 +1,40 @@
+﻿using NorthWind.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthWind.Controllers
+{
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Commande/
+
+        public ActionResult Index()
+        {
+            using (var dao = new Entities())
+            {
+
+                OrderRepository orderRepository = new OrderRepository(dao);
+                List<Order> orders = orderRepository.FindAllOrders().ToList();
+
+                return View(orders);
+            }
+            
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var dao = new Entities())
+            {
+                OrderRepository orderRepository = new OrderRepository(dao);
+                Order order = orderRepository.GetOrder(id);
+
+                return View(order);
+            }
+        }
+
+    }
+}
---
+++ NorthWind/NorthWind/Models/EmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NorthWind.Models
+{
+    public class EmployeeRepository
+    {
+        private Entities db;
+
+        public EmployeeRepository(Entities entities) { this.db = entities; }
+
+        public Employee GetEmployee(int id)
+        {
+            return db.Employees.SingleOrDefault(employee => employee.EmployeeID == id);
+        }
+    }
+}
