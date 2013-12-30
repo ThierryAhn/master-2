698db6490f8746aaa663122294fe93505877bece
folabi41@gmail.com <folabi41@gmail.com@73b4ffdc-115d-2568-caed-99f011975a9d>
2013-12-30 17:27:54 +0000

    Import initial

---
+++ trunk/NorthWind/NorthWind/Models/CustomerRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NorthWind.Models
+{
+    public class CustomerRepository
+    {
+        private Entities db;
+
+        public CustomerRepository(Entities entities) { this.db = entities; }
+
+        public Customer GetCustomer(string id)
+        {
+            return db.Customers.SingleOrDefault(customer => customer.CustomerID == id);
+        }
+
+        public IQueryable<Customer> FindAllCustomers()
+        {
+            return db.Customers;
+        }
+
+        public void Add(Customer customer)
+        {
+        }
+
+        public void Delete(Customer customer)
+        {
+        }
+
+        public void Save() { db.SaveChanges(); }
+    }
+}
