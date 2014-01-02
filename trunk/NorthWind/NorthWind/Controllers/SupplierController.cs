e24d6bf45b2357fce63c1312e5745ef24a3ecc9d
folabi41@gmail.com <folabi41@gmail.com@73b4ffdc-115d-2568-caed-99f011975a9d>
2014-01-02 22:52:49 +0000

    Gestion de CRUD pour les fournisseurs et modification mineure dans Produit

--- trunk/NorthWind/NorthWind/Controllers/SupplierController.cs
+++ trunk/NorthWind/NorthWind/Controllers/SupplierController.cs
@@ -42,5 +42,135 @@ namespace NorthWind.Controllers
             }
         }
 
+        // GET Create Supplier
+        public ActionResult Create()
+        {
+            return View(new EditableSupplier());
+        }
+
+        [HttpPost]
+        public ActionResult Create(EditableSupplier editableSupplier)
+        {
+            using (var dao = new Entities())
+            {
+                if (ModelState.IsValid)
+                {
+                    SupplierRepository supplierRepository = new SupplierRepository(dao);
+                    Supplier supplier = new Supplier();
+
+                    supplier.CompanyName = editableSupplier.CompanyName;
+                    supplier.ContactName = editableSupplier.ContactName;
+                    supplier.ContactTitle = editableSupplier.ContactTitle;
+                    supplier.Address = editableSupplier.Address;
+                    supplier.City = editableSupplier.City;
+                    supplier.Region = editableSupplier.Region;
+                    supplier.PostalCode = editableSupplier.PostalCode;
+                    supplier.Country = editableSupplier.Country;
+                    supplier.Phone = editableSupplier.Phone;
+                    supplier.Fax = editableSupplier.Fax;
+                    supplier.HomePage = editableSupplier.HomePage;
+
+                    supplierRepository.Add(supplier);
+                    supplierRepository.Save();
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(editableSupplier);
+                }
+            }
+        }
+
+
+        // GET Edit Supplier
+        public ActionResult Edit(int id)
+        {
+            using (var dao = new Entities())
+            {
+                SupplierRepository supplierRepository = new SupplierRepository(dao);
+                Supplier supplier = supplierRepository.GetSupplier(id);
+
+                EditableSupplier editableSupplier = new EditableSupplier(supplier);
+
+                return View(editableSupplier);
+            }
+        }
+
+        // POST Edit Supplier
+        [HttpPost]
+        public ActionResult Edit(EditableSupplier editableSupplier)
+        {
+            using (var dao = new Entities())
+            {
+                if (ModelState.IsValid)
+                {
+                    SupplierRepository supplierRepository = new SupplierRepository(dao);
+
+                    Supplier supplier = supplierRepository.GetSupplier(editableSupplier.SupplierID);
+
+                    supplier.CompanyName = editableSupplier.CompanyName;
+                    supplier.ContactName = editableSupplier.ContactName;
+                    supplier.ContactTitle = editableSupplier.ContactTitle;
+                    supplier.Address = editableSupplier.Address;
+                    supplier.City = editableSupplier.City;
+                    supplier.Region = editableSupplier.Region;
+                    supplier.PostalCode = editableSupplier.PostalCode;
+                    supplier.Country = editableSupplier.Country;
+                    supplier.Phone = editableSupplier.Phone;
+                    supplier.Fax = editableSupplier.Fax;
+                    supplier.HomePage = editableSupplier.HomePage;
+
+                    UpdateModel(supplier);
+
+                    supplierRepository.Save();
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(editableSupplier);
+                }
+            }
+        }
+
+
+        // GET Delete Product
+        public ActionResult Delete(int id)
+        {
+            using (var dao = new Entities())
+            {
+                SupplierRepository supplierRepository = new SupplierRepository(dao);
+                Supplier supplier = supplierRepository.GetSupplier(id);
+
+                if (supplier == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(supplier);
+            }
+        }
+
+        // POST Delete Product
+        [HttpPost]
+        public ActionResult Delete(int id, String action)
+        {
+            using (var dao = new Entities())
+            {
+                SupplierRepository supplierRepository = new SupplierRepository(dao);
+                Supplier supplier = supplierRepository.GetSupplier(id);
+
+                if (supplier == null)
+                {
+                    return HttpNotFound();
+                }
+
+                supplierRepository.Delete(supplier);
+                supplierRepository.Save();
+                
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
---
+++ trunk/NorthWind/NorthWind/Models/EditableSupplier.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthWind.Models
+{
+    public class EditableSupplier
+    {
+        public int SupplierID { get; set; }
+        [Display(Name = "Nom de la compagnie")]
+        public string CompanyName { get; set; }
+        [Display(Name = "Nom du contact")]
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        [Display(Name = "Adresse")]
+        public string Address { get; set; }
+        [Display(Name = "Ville")]
+        public string City { get; set; }
+        [Display(Name = "Region")]
+        public string Region { get; set; }
+        [Display(Name = "Code postal")]
+        public string PostalCode { get; set; }
+        [Display(Name = "Pays")]
+        public string Country { get; set; }
+        [Display(Name = "Téléphone")]
+        public string Phone { get; set; }
+        [Display(Name = "Fax")]
+        public string Fax { get; set; }
+        [Display(Name = "Site web")]
+        public string HomePage { get; set; }
+
+
+        public List<SelectListItem> Contacts
+        {
+            get
+            {
+                using (var dao = new Entities())
+                {
+                    List<SelectListItem> items = new List<SelectListItem>();
+                    foreach (Contact contact in dao.Contacts)
+                    {
+                        if (contact.ContactName != null)
+                        {
+                            items.Add(new SelectListItem
+                            {
+                                Text = contact.ContactName,
+                                Value = contact.ContactName
+                            });
+                        }
+                        
+                    }
+                    return items;
+                }
+            }
+        }
+
+        public EditableSupplier()
+        {
+        }
+
+        public EditableSupplier(Supplier supplier)
+        {
+            this.SupplierID = supplier.SupplierID;
+            this.CompanyName = supplier.CompanyName;
+            this.ContactName = supplier.ContactName;
+            this.ContactTitle = supplier.ContactTitle;
+            this.Address = supplier.Address;
+            this.City = supplier.City;
+            this.Region = supplier.Region;
+            this.PostalCode = supplier.PostalCode;
+            this.Country = supplier.Country;
+            this.Phone = supplier.Phone;
+            this.Fax = supplier.Fax;
+            this.HomePage = supplier.HomePage;
+        }
+    }
+}
