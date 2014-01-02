96ea4b848e259f0e5535d53ee81fdcd01555a174
folabi41@gmail.com <folabi41@gmail.com@73b4ffdc-115d-2568-caed-99f011975a9d>
2014-01-02 03:17:26 +0000

    Finition du controleur de category + sa vue details

---
+++ trunk/NorthWind/NorthWind/Controllers/CategoryController.cs
@@ -0,0 +1,51 @@
+﻿using NorthWind.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthWind.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //
+        // GET: /Category/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // Details
+
+        public ActionResult Details(int id)
+        {
+            using (var dao = new Entities())
+            {
+                CategoryRepository categoryRepository = new CategoryRepository(dao);
+                Category category = categoryRepository.GetCategory(id);
+                return View(category);
+            }
+        }
+
+        public ActionResult GetImage(int id)
+        {
+            using (var dao = new Entities())
+            {
+                using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+                {
+                    CategoryRepository categoryRepository = new CategoryRepository(dao);
+                    Category category = categoryRepository.GetCategory(id);
+                    var image = category.Picture;
+                    // 78 is the size of the OLE header for Northwind images
+                    ms.Write(image, 78, image.Length - 78);
+
+                    return File(ms.ToArray(), "image/jpeg");
+                }
+            }
+        }
+
+    }
+}
